0928b07d137e2da1e39db06381c0660d11294a89
Jason <jacklin953107@gmail.com>
2017-12-23 23:49:29 +0800

    modify the _restaurant_list view 2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def show_img(img, exist)
+    if exist
+      for img_id in 0..7
+        include_path = '/'+img_id.to_s+'.jpg'
+        return image_tag(('/seed_img'+include_path), class: "img-responsive center-block") if image_path(img).include?(include_path)
+      end
+    end
+    '<span class="glyphicon glyphicon-picture"></span>[沒有圖片]'.html_safe
+  end
 end
