ac24c161e12ba4a2107e0d1f7cd8efbbe57bf308
Jason <jacklin953107@gmail.com>
2017-12-28 22:28:53 +0800

    styled profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates_presence_of :name
+  has_many :restaurants, through: :comments
   mount_uploader :avatar, AvatarUploader
   def admin?
     self.role == "admin"
